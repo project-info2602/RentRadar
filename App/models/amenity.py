10cd0a01e2090e2c58d7e212206d8cdd8ef42d6d
Jaheem Edwards <130275590+jaheemedwards@users.noreply.github.com>
2025-03-30 03:15:59 -0400

    Updates to the Amenity model.

--- App/models/amenity.py
+++ App/models/amenity.py
@@ -7,16 +7,29 @@ class Amenity(db.Model):
     def __repr__(self):
         return f"<Amenity {self.name}>"
 
+    def get_json(self):
+        """Returns the amenity information as a dictionary."""
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
 class ApartmentAmenity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     apartment_id = db.Column(db.Integer, db.ForeignKey('apartment.id'), nullable=False)
     amenity_id = db.Column(db.Integer, db.ForeignKey('amenity.id'), nullable=False)
 
+    apartment = db.relationship('Apartment', backref='apartment_amenities', lazy=True)
+    amenity = db.relationship('Amenity', backref='apartment_amenities', lazy=True)
+
     def __repr__(self):
         return f"<ApartmentAmenity Apartment {self.apartment_id} - Amenity {self.amenity_id}>"
 
     def get_json(self):
+        """Returns apartment-amenity relationship as a dictionary with amenity name."""
         return {
             "id": self.id,
-            "name": self.name
-    }
+            "apartment_id": self.apartment_id,
+            "amenity_id": self.amenity_id,
+            "amenity_name": self.amenity.name  # Get amenity's name
+        }
