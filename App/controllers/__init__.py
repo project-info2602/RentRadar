000b74bf490d81ac7fbe9735131aa84a4b807ade
Jaheem Edwards <130275590+jaheemedwards@users.noreply.github.com>
2025-04-02 03:47:23 -0400

    Replaced the Amenity class with a list of amenities. Also added the list of locations.

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,6 +1,5 @@
 from .user import *
 from .auth import *
 from .apartment import *
-from .amenity import *
 from .review import *
 from .initialize import *
--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,4 +1,3 @@
 from .user import User
 from .apartment import Apartment
-from .amenity import Amenity, ApartmentAmenity
 from .review import Review
