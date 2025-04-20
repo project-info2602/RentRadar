7f00d09ea9c54d16976509d5c1c310b62f994721
Shonzino <yoleshon@outlook.com>
2025-04-19 20:58:27 -0400

    closing main routes

--- App/main.py
+++ App/main.py
@@ -442,4 +442,10 @@ def create_app():
                             amenities=AMENITIES)
 
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
        
